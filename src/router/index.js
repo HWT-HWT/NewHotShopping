047a0e5c8f05cf77236878ff9d31d43c6483947e
CtTow <2331938324@qq.com>
2024-07-12 12:00:19 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login/MyLogin.vue'
+import Layout from '@/views/Layout/MyLayout.vue'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'Layout',
+    component: Layout
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Layout/MyLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="MyLayout">
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout'
+}
+</script>
+
+<style>
+
+</style>
